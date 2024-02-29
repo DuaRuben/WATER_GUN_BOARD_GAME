45cef483755d05e6c5f16c69c4a607bf80ae9198
DuaRuben <147565092+DuaRuben@users.noreply.github.com>
2024-02-29 15:39:25 -0800

    Board Manager Changes

--- src/Model/BoardManager.java
+++ src/Model/BoardManager.java
@@ -1,8 +1,25 @@
 package src.Model;
 
+import java.util.ArrayList;
+
 public class BoardManager {
     //Manages the array of board
+    private ArrayList<ArrayList<Cell>> board = new ArrayList<>();
+
+    public ArrayList<ArrayList<Cell>> getBoard() {
+        return board;
+    }
+
+    //board creation
+
     //prints the board
+    public void printBoard(){
+        for(int i=0;i<10;i++){
+            for(int j=0;j<10;j++){
+                System.out.println(board.get(i).get(j).getValue());
+            }
+        }
+    }
     //prints the cheat board
     //returns hit or miss on userInput
 }
