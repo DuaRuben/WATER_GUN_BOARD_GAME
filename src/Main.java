e977b234f7e3140706e8a93ec6623d95f033b8f9
prathamgarg03 <gargpratham.312003@gmail.com>
2024-02-29 20:48:32 -0800

    Fort class

--- src/Main.java
+++ src/Main.java
@@ -8,21 +8,23 @@ import java.util.Objects;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int forts = 0;
+        int forts = 5;
         boolean cheat = false;
-        if(args.length <= 0 || args.length > 2) {
-            System.err.println("Invalid Input");
-            System.exit(1);
-        } else {
-            try {
-                forts = Integer.parseInt(args[0]);
-            } catch (Exception e) {
-                System.err.println("Number of forts must be an integer");
+        if(args.length != 0) {
+            if(args.length > 2) {
+                System.err.println("Invalid Input");
                 System.exit(1);
+            } else {
+                try {
+                    forts = Integer.parseInt(args[0]);
+                } catch (Exception e) {
+                    System.err.println("Number of forts must be an integer");
+                    System.exit(1);
+                }
             }
-        }
-        if(args.length == 2 && Objects.equals(args[1], "--cheat")) {
-            cheat = true;
+            if(args.length == 2 && Objects.equals(args[1], "--cheat")) {
+                cheat = true;
+            }
         }
         TextUI UI = new TextUI();
         UI.start(forts, cheat);
