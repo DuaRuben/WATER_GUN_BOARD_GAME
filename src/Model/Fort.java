98d23eefce9e968c22af436aa72fd0a18d6b1d82
DuaRuben <147565092+DuaRuben@users.noreply.github.com>
2024-02-29 16:30:08 -0800

    Update Fort.java

--- src/Model/Fort.java
+++ src/Model/Fort.java
@@ -1,5 +1,59 @@
 package src.Model;
 
+import java.util.ArrayList;
+
 public class Fort {
-    
+    private String fortName;
+    private ArrayList<Cell> cellArray;
+    private boolean isDestroyed;
+    private int undamagedCells;
+    private int points;
+
+    public Fort(String fortName, ArrayList<Cell> cellArray, boolean isDestroyed, int undamagedCells, int points) {
+        this.fortName = fortName;
+        this.cellArray = cellArray;
+        this.isDestroyed = isDestroyed;
+        this.undamagedCells = undamagedCells;
+        this.points = points;
+    }
+
+    public String getFortName() {
+        return fortName;
+    }
+
+    public ArrayList<Cell> getCellArray() {
+        return cellArray;
+    }
+
+    public boolean isDestroyed() {
+        return isDestroyed;
+    }
+
+    public int getUndamagedCells() {
+        return undamagedCells;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setFortName(String fortName) {
+        this.fortName = fortName;
+    }
+
+    public void setCellArray(ArrayList<Cell> cellArray) {
+        this.cellArray = cellArray;
+    }
+
+    public void setDestroyed(boolean destroyed) {
+        isDestroyed = destroyed;
+    }
+
+    public void setUndamagedCells(int undamagedCells) {
+        this.undamagedCells = undamagedCells;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
 }
