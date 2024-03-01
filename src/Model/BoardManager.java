ee140439c58abee0c15a212d82d31834ff5a2ffe
DuaRuben <147565092+DuaRuben@users.noreply.github.com>
2024-02-29 16:37:22 -0800

    Update BoardManager.java

--- src/Model/BoardManager.java
+++ src/Model/BoardManager.java
@@ -14,12 +14,46 @@ public class BoardManager {
 
     //prints the board
     public void printBoard(){
-        for(int i=0;i<10;i++){
-            for(int j=0;j<10;j++){
-                System.out.println(board.get(i).get(j).getValue());
+        int col = 1;
+        char row = 'A';
+        for(int i=0;i<11;i++){
+            for(int j=0;j<11;j++){
+                if( i == 0){
+                    System.out.println(col);
+                    col++;
+                }
+                else if( j == 0){
+                    System.out.println(row);
+                    row++;
+                }
+                else{
+                    System.out.println(board.get(i-1).get(j-1).getValue());
+                }
             }
         }
     }
     //prints the cheat board
+    public void printCheatBoard(){
+        int col = 1;
+        char row = 'A';
+        for(int i=0;i<11;i++){
+            for(int j=0;j<11;j++){
+                if( i == 0){
+                    System.out.println(col);
+                    col++;
+                }
+                else if( j == 0){
+                    System.out.println(row);
+                    row++;
+                }
+                else if(board.get(i-1).get(j-1).getFortName().isEmpty()){
+                    System.out.println(".");
+                }
+                else{
+                    System.out.println(board.get(i-1).get(j-1).getFortName());
+                }
+            }
+        }
+    }
     //returns hit or miss on userInput
 }
